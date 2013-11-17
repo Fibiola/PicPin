dfb6d09d8a1ae3e3789382001da0a3b98aba3a00
Fibiola <natasha.pristovsek@gmail.com>
2013-11-18 00:48:23 +0100

    adding the app and gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinPic::Application.config.secret_key_base = 'e7d220a1146c92881a3e8512a85147039711710eb3ed97cbe1f437bfe8f9b4f641c7abbac2a115916024805925e82c696c6bf42cc6ad5324b5105daff584cf50'
