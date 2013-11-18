4136d4d6164e46922b2c87ef7bbd77f1a384a907
Fibiola <natasha.pristovsek@gmail.com>
2013-11-18 18:43:46 +0100

    modified pages and about view, created partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-PinPic::Application.routes.draw do
-  get "pages/about"
+PinPic::Application.routes.draw do 
+  get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
-
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
