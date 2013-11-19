09a122471c114cc65ae4fb63cee73c61676b49ef
Fibiola <natasha.pristovsek@gmail.com>
2013-11-19 02:58:23 +0100

    added sign up functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ PinPic::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # devise - we send email to users - so it needs to know our url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
